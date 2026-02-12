1d46633edba57afb3937314e7fccffdf3d21d946
wer16 <wer16@DESKTOP-RV2RFCP>
2026-02-12 16:31:26 +0300

    Доделал пз4 по тестированию, переделал pz2 по мобильной разработке

---
+++ Мобильные разработки/pz_2/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace pz_2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ProgressBarButton(object sender, EventArgs e)
+        {
+            Progress.Progress = 0;
+            for(int i = 0; i <= 100; i++)
+            {
+                Progress.IsVisible = true;
+                Progress.Progress = i / 100.0;
+                label1.Text = $"Состояние процесса: {i} %";
+                await Task.Delay(30);
+            }
+        }
+    }
+}
