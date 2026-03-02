ad339bf3dd997e4b0c310b7cf964c9249613fc60
wer16 <wer16@DESKTOP-RV2RFCP>
2026-03-02 13:22:20 +0300

    pz 7 Доделать

---
+++ Мобильные разработки/pz7/prikol/models/Cards.cs
@@ -0,0 +1,10 @@
+﻿namespace prikol.models
+{
+    public class Cards
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
---
+++ Мобильные разработки/pz7/prikol/operations/Operations.cs
@@ -0,0 +1,40 @@
+﻿using prikol.models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace prikol.operations
+{
+    internal static class Operations
+    {
+        public static ObservableCollection<Cards> AllCards { get; set; }
+        static Operations()
+        {
+            AllCards = new ObservableCollection<Cards>()
+            {
+                new Cards()
+                {
+                    Id = 1,
+                    Name = "Магнит",
+                    Image = "carta.png",
+                    Number = "12341241124315"
+                },
+                new Cards()
+                {
+                    Id = 2,
+                    Name = "Пятерочка",
+                    Image = "carta.png",
+                    Number = "1234124115477"
+                },
+                new Cards()
+                {
+                    Id = 3,
+                    Name = "Дикси",
+                    Image = "carta.png",
+                    Number = "12345645724315"
+                }
+            };
+        }
+    }
+}
