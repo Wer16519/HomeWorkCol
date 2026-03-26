4c2911a60f6575176b998fa21372b4399722be1e
wer16 <wer16@DESKTOP-RV2RFCP>
2026-03-26 17:20:09 +0300

    pz15, pz19, pz20 сис прог

--- Системное программирование/PZ19/PZ19/PZ19/obj/Debug/net10.0/PZ19.AssemblyInfo.cs
+++ Системное программирование/PZ19/PZ19/PZ19/obj/Debug/net10.0/PZ19.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PZ19")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cffea8f9b0a43a038c563e1bfae1ae331c36dce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PZ19")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PZ19")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
